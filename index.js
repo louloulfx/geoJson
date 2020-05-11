809c8de3f2ca7866a5944595cb401bd3e0ffd972
Elouan <louloulfx1@hotmail.fr>
2020-05-11 15:19:24 +0200

    étape 1

---
+++ index.js
@@ -0,0 +1,23 @@
+db.place.insertOne({
+  name: "10ème arrondissement",
+  geometry: {
+    type: "Point",
+    coordinates: [2.3594856262207027, 48.876288295456625],
+  },
+});
+
+db.place.insertOne({
+  name: "14ème arrondissement",
+  geometry: {
+    type: "Point",
+    coordinates: [2.326784133911133, 48.83342459901093],
+  },
+});
+
+db.place.insertOne({
+  name: "8ème arrondissement",
+  geometry: {
+    type: "Point",
+    coordinates: [2.3169994354248047, 48.87758662245016],
+  },
+});
